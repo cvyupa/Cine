10e09d089f5803765ab00fcf8d3377a105368026
cvyupa <32906705+cvyupa@users.noreply.github.com>
2017-11-15 10:22:20 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
+/*
+   Universidad de las Fuerzas Armadas ESPE
+   Proyecto Primer Parcial
+   Cine
+   Integrantes:
+Carlos Yupa
+Mauricio Ruiz
+Andres Sanaguano
+*/
 #include <iostream>
 #include<stdio.h>
 using namespace std;
